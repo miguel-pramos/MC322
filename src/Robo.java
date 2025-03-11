e27f1cea1ef2a2e406f16eb7bbccae39511e0215
rafael-tagliamento <taglirafael.com>
2025-03-11 15:22:47 -0300

    elements from robot class

--- src/Robo.java
+++ src/Robo.java
@@ -2,4 +2,23 @@ public class Robo {
     private String nome;
     private int posicaoX;
     private int posicaoY;
+
+    public Robo(String nome, int posicaoX, int posicaoY)
+    {
+        this.nome = nome;
+        this.posicaoX = posicaoX;
+        this.posicaoY = posicaoY;
+    }
+
+    public void mover(int deltaX, int deltaY)
+    {
+        this.posicaoX += deltaX;
+        this.posicaoY += deltaY;
+    }
+
+    public int exibirPosicao()
+    {
+        return this.posicaoX;
+        return this.posicaoY;
+    }
 }
