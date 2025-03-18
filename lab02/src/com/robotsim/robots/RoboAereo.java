21d031daa4546595929dc06f5f39f29b24ca4d5a
Miguel Ramos <m281335@dac.unicamp.br>
2025-03-18 14:51:45 -0300

    new robot classes init

---
+++ lab02/src/com/robotsim/robots/RoboAereo.java
@@ -0,0 +1,10 @@
+package com.robotsim.robots;
+
+public class RoboAereo extends Robo {
+
+    public RoboAereo(String nome, int posicaoX, int posicaoY) {
+        super(nome, posicaoX, posicaoY);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
---
+++ lab02/src/com/robotsim/robots/RoboTerrestre.java
@@ -0,0 +1,10 @@
+package com.robotsim.robots;
+
+public class RoboTerrestre extends Robo {
+
+    public RoboTerrestre(String nome, int posicaoX, int posicaoY) {
+        super(nome, posicaoX, posicaoY);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
