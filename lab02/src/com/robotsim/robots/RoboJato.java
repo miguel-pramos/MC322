4e2e3bda8eab5b492fa41d24391e4a135c6a85a6
rafael-tagliamento <taglirafael.com>
2025-03-26 10:40:09 -0300

    testing new archives disposition

--- lab02/src/com/robotsim/robots/RoboJato.java
+++ lab02/src/com/robotsim/robots/RoboJato.java
@@ -1,45 +1,22 @@
-// package com.robotsim.robots;
+package com.robotsim.robots;
 
-// import java.io.BufferedReader;
-// import java.io.IOException;
-// import java.io.InputStreamReader;
+import com.robotsim.util.GeometryMath;
 
-// import com.robotsim.robots.RoboAereo;
+public class RoboJato extends RoboAereo {
+    private int misseisRestantes = 4;
+    private int rajadasRestantes = 10;
+    private int HP = 100;
 
-// public class RoboJato extends RoboAereo {
+    public RoboJato(String nome, int posicaoX, int posicaoY, int altitude, int altitudeMaxima) {
 
-//     public RoboJato(String nome, int posicaoX, int posicaoY, int altitude, int altitudeMaxima) {
-//         super(nome, posicaoX, posicaoY, altitude, altitudeMaxima);
-//         char answer;
+    }
 
-//         public void subir(char answer, int deltaAltitude){
-//             if (this.altitude + deltaAltitude > this.altitudeMaxima){
-//                 while (1)`{
-//                     System.out.println("Essa altitude ultrapassará os limites do jato. Deseja mesmo continuar? Y/N");
-//                     try {
-//                         this.answer = System.in.read();
-//                     } catch (IOException e) {
-//                         System.out.println("Ocorreu um erro com a entrada.");
-//                         e.printStackTrace();
-//                         continue;
-//                     }
+    public void lancarMissil(RoboAereo alvo){
+        if (GeometryMath.distanciaEuclidiana(alvo.getPosicaoX(), getPosicaoY(), getPosicaoZ()));
+    }
 
-//                     switch (answer) {
-//                         case 'y': {
-//                             System.out.println("Você subiu... e subiu... até não aguentar mais");
-//                             gameStatus = GAME_STATUS.GAMEOVER;
-//                             break;
-//                         }
-//                         case 'n': {
-//                             System.out.println("Você subiu para a posição (" + posicaoX + ", " + posicaoY + ", " + altitude + ").");
-//                             break;
-//                         }
-//                         default:
-//                             System.out.println("Ocorreu um erro com a entrada.");
-//                     }
-//                 }
-//             }
-//         }
-
-//     }
-// }
+    public void lancarMissil(RoboTerrestre alvo){
+        if(GeometryMath.distanciaEuclidiana){
+            if (GeometryMath.distanciaEuclidiana(alvo.getPosicaoX(), getPosicaoY(), 0));
+        }
+}
--- lab02/src/com/robotsim/util/GeometryMath.java
+++ lab02/src/com/robotsim/util/GeometryMath.java
@@ -1,9 +1,12 @@
 package com.robotsim.util;
 
 public class GeometryMath {
-    
+
     public static double distanciaEuclidiana(int x, int y) {
         return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
     }
-    
+
+    public static double distanciaEuclidiana(double x, double y) {
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+    }
 }
