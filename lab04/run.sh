2813140d0b01acd56cd19bde4a649a360df97f8c
Miguel Ramos <m281335@dac.unicamp.br>
2025-05-28 17:10:09 -0300

    new script

--- lab04/run.sh
+++ lab04/run.sh
@@ -1,3 +1,13 @@
-rm -r ./bin/
-javac -sourcepath src src/com/robotsim/Controlador.java -d bin
-java -cp bin com.robotsim.Controlador
+#!/bin/bash
+rm -rf ./bin/* 
+mkdir -p ./bin 
+
+# Encontra todos os arquivos .java em src/ e os compila para o diretório bin
+find src -name "*.java" -print0 | xargs -0 javac -d bin -sourcepath src
+
+# Verifica se a compilação foi bem-sucedida antes de executar
+if [ $? -eq 0 ]; then
+    java -cp bin com.robotsim.Controlador
+else
+    echo "Erro de compilação. Verifique os erros acima."
+fi
